60b9c2115528ef5e5c30e046ac0f9e86f7c7768d
Stanislau Charniakou <stanislau_charniakou@epam.com>
2013-05-23 12:21:37 +0300

    added daterange filter

--- feed.py
+++ feed.py
@@ -1,6 +1,6 @@
 from models import Article, to_dict
 from handler import BseuEmbedHandler
-
+from datetime import datetime
 
 class NewsFeed(BseuEmbedHandler):
 
@@ -9,9 +9,15 @@ class NewsFeed(BseuEmbedHandler):
         Simple offset-based endless json/jsonp feed
         """
 
-        articles = Article.all().order('-publish_date').filter('publish_state = ',
-                                                               True).fetch(10,
-                                                                           offset=int(self.request.get('offset', 0)))
+        articles = Article.all().filter('publish_state = ', True)\
+
+        if self.request.get('start') and self.request.get('end'):
+            articles = articles.filter('publish_date >=',
+                                       datetime.strptime(self.request.get('start'), '%d/%m/%Y')
+                              ).filter('publish_date <=',
+                                       datetime.strptime(self.request.get('end'), '%d/%m/%Y'))
+
+        articles = articles.order('-publish_date').fetch(10, offset=int(self.request.get('offset', 0)))
         articles = [to_dict(item) for item in articles]
 
         callback = self.request.get('callback')
